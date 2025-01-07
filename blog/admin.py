bd5725a9e6133148e1ecfe699252ea9374b1ca4d
Douglas Mottershead <douglas.mottershead@outlook.com>
2025-01-07 16:18:16 +0000

    add summernote

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 from .models import comment
+from django_summernote.admin import SummernoteModelAdmin
 # Register your models here.
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
+# admin.site.register(Post)
 admin.site.register(comment)
--- codestar/urls.py
+++ codestar/urls.py
@@ -18,5 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
+    
+    
 ]
